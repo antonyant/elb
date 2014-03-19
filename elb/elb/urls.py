bab7ccd4117586e631866b1699b8bb4f35059969
antonyant <ant@aewstudios.com>
2014-03-19 13:24:55 +1100

    added splash page

--- elb/elb/urls.py
+++ elb/elb/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.generic import TemplateView
-
 from django.views.generic import RedirectView
 
 from django.contrib import admin
@@ -12,13 +10,12 @@ from views import galleries, galleries_tag
 
 urlpatterns = patterns('',
 
-    url(r'^$', TemplateView.as_view(template_name='base.html')),
+    (r'^$', RedirectView.as_view(url='/home/')),
     url(r'^photologue/', include('photologue.urls')),
     (r'^papers/', include('editorials.urls')), 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     (r'^tinymce/', include('tinymce.urls')),
-    (r'^$', RedirectView.as_view(url='/home/')),
     (r'^galleries/type/(?P<tag>[-\w]+)/$', galleries_tag),
     (r'^galleries/(?P<tag>[-\w]+)/(?P<slug>[-\w]+)/$', galleries),
 
